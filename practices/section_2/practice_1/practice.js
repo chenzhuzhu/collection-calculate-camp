7b7d5263d42fedbab132a87baa868ccb63ebaf77
陈昱竹 <826537618@qq.com>
2017-08-13 22:42:23 +0800

    save some

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,26 @@
+'use strict';
+function find(collection,element){
+  for(let item of collection){
+    if(item['key']==element){
+      return item;
+    }
+  }
+}
+
 function count_same_elements(collection) {
-  //在这里写入代码
+  let result = [];
+
+  for(let item of collection){
+    let finded = find(result,item)
+    if(finded){
+      finded['count']++;
+    }else{
+      result.push({key:item,count:1})
+    }
+
+  }
+  return result;
+
 }
 
 module.exports = count_same_elements;
--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,15 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let result=[];
+  for(let item of collection) {
+    if(item.length!=1){
+      let key=item.split('-')[0]
+      let count=item.split('-')[1]
+      let {key,count};
+      result.push({key,count})
+
+
+    }
+  }
 }
 
 module.exports = count_same_elements;
