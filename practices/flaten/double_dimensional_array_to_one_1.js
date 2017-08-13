3e8a972589066896643cbce49f40e534b3d8131c
陈昱竹 <826537618@qq.com>
2017-08-13 16:21:55 +0800

    flaten finished

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,24 @@
 'use strict';
 
-function double_to_one(collection) {
+function toElement(array,result){
+  array.map(function(item){
+    result.push(item)
+  })
+  return result
 
-  //在这里写入代码
+}
+
+function double_to_one(collection) {
+  let result=[];
+  for(let item of collection){
+    if(Array.isArray(item)){
+      result = toElement(item,result)
+    }else{
+      result.push(item);
+    }
+  }
+  console.log(result)
+  return result;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,40 @@
 'use strict';
 
+function toElement(array,result){
+  array.map(function(item){
+    result.push(item)
+  })
+  return result
+}
+
+function toOneDimension(collection,result){
+  for(let item of collection){
+    if(Array.isArray(item)){
+      result = toElement(item,result)
+    }else{
+      result.push(item);
+    }
+  }
+  return result;
+
+}
+
+function include(result,temp_result){
+  for(let temp_item of temp_result){
+    if (!result.includes(temp_item)){
+      result.push(temp_item);
+    }
+  }
+  return result;
+}
 function double_to_one(collection) {
 
-  //在这里写入代码
+  let temp_result=[];
+  let result=[];
+  temp_result=toOneDimension(collection,temp_result);
+  result =include(result,temp_result);
+  return result;
+
 }
 
 module.exports = double_to_one;
