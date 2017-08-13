82171df30fb5caeddca8a5f2382f179f39d56d1f
陈昱竹 <826537618@qq.com>
2017-08-14 00:27:25 +0800

    save some

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,15 +1,54 @@
-function count_same_elements(collection) {
+'use strict';
+function split(element){
+  let splited = element.split('-');
+  return {key:splited[0],count:parseInt(splited[1],10)};
+
+}
+function push(result,key,value){
+  for(let i=0;i<value;i++){
+    result.push(key)
+  }
+  return result;
+}
+
+function expand(collection){
   let result=[];
-  for(let item of collection) {
-    if(item.length!=1){
-      let key=item.split('-')[0]
-      let count=item.split('-')[1]
-      let {key,count};
-      result.push({key,count})
-
-
+  for(let item of collection){
+    if(item.length==1){
+      result.push(item);
+    }else{
+      let {key,count} = split(item);
+      push(result,key,count)
     }
   }
+  return result;
+}
+
+function find(collection,element){
+  for(let item of collection){
+    if(element == item['key']){
+      return item;
+    }
+  }
+  return false;
+}
+
+function summarize(collection){
+  let result=[];
+  for(let item of collection){
+    let finded =find(result,item);
+    if (finded){
+      finded.count++;
+    }else{
+      result.push({key:item,count:1})
+    }
+  }
+  return result;
+}
+
+function count_same_elements(collection) {
+  let expanded = expand(collection)
+  return summarize(expanded);
 }
 
 module.exports = count_same_elements;
