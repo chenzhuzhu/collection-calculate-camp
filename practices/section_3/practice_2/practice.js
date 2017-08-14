0ea8e63a2ad1c254edfa4065a95fd6770f36c1fb
陈昱竹 <826537618@qq.com>
2017-08-14 11:38:14 +0800

    section_3 save some

--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,5 +1,25 @@
+'use strict';
+
+function find(collection,element){
+  for(let item of collection){
+    if(item==element['key']){
+      return element
+    }
+  }
+}
+
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let result=[];
+  for(let item of collection_a){
+    let finded = find(object_b['value'],item);
+    if(finded){
+      finded.count=finded.count - Math.floor(finded.count/3);
+      result.push(finded);
+    }else{
+      result.push(item)
+    }
+  }
+  return collection_a;
 }
 
 module.exports = create_updated_collection;
--- practices/section_3/practice_3/practice.js
+++ practices/section_3/practice_3/practice.js
@@ -1,5 +1,52 @@
+'use strict'
+
+function find(collection,element){
+  for(let item of collection){
+    if (item['key']==element){
+      return item
+    }
+  }
+  return false;
+}
+
+function summary(collection){
+  let result =[];
+  for(let item of collection){
+    let finded = find (result,item)
+    if(finded){
+      finded.count++
+    }else{
+      result.push({key:item,count:1});
+    }
+  }
+  return result;
+}
+
+function findObject(object,element){
+  for(let item of object){
+    if (item == element['key']){
+      return element
+    }
+  }
+  return false
+}
+
+function compare(collection,object){
+  let result =[];
+  for (let item of collection){
+    let finded= findObject(object,item)
+    if(finded){
+      finded.count= finded.count-Math.floor(finded.count/3)
+      result.push(finded);
+    }else{
+      result.push(item)
+    }
+  }
+  return result;
+}
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let summarized = summary(collection_a)
+  return compare(summarized,object_b['value'])
 }
 
 module.exports = create_updated_collection;
