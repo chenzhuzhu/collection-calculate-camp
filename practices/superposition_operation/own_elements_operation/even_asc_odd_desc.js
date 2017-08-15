fee2a21f32c766b164fb02377d96fcee56f304db
陈昱竹 <826537618@qq.com>
2017-08-15 10:04:38 +0800

    task4 finished

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,65 @@
 'use strict';
+function isEven(num){
+  if(num%2==0){
+    return true;
+  }
+  return false;
+}
+
+function divideArray(collection){
+  let result=[];
+  let oddArray=[];
+  let evenArray=[];
+  for(let item of collection){
+    if(isEven(item)){
+      evenArray.push(item);
+    }else{
+      oddArray.push(item);
+    }
+  }
+  result.push(evenArray);
+  result.push(oddArray);
+  return result;
+}
+function compareEven(value1,value2){
+  if (value1<value2){
+    return -1
+  }else if(value1>value2){
+    return 1
+  }else{
+    return 0
+  }
+}
+function compareOdd(value1,value2){
+  if(value1>value2){
+    return -1
+  }else if(value1<value2){
+    return 1
+  }else{
+    return 0
+  }
+}
+
+function sortArray(collection){
+  let result=[];
+  collection[0].sort(compareEven);
+  collection[1].sort(compareOdd);
+  for (let each_collection of collection){
+    for(let item of each_collection){
+      result.push(item)
+    }
+  }
+  return result;
+
+}
+
+
 var even_asc_odd_desc = function(collection){
+  let divided = divideArray(collection)
+  return sortArray(divided)
+
+
+
 
 };
 module.exports = even_asc_odd_desc;
--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,31 @@
 'use strict';
+function compare(value1,value2){
+  if(value1<value2){
+    return -1
+  }else if(value1>value2){
+    return 1
+  }else{
+    return 0
+  }
+}
+function toRank(collection){
+  let result=[];
+  for(let i=0;i<collection.length-(collection.length%3);i=i+3){
+    result.push(collection[i+1])
+    result.push(collection[i+2])
+    result.push(collection[i])
+  }
+  if(collection.length%3>0){
+    for(let i=collection.length%3;i>0;i--){
+      result.push(collection[(collection.length-i)])
+    }
+  }
+
+  return result;
+}
 function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+  collection.sort(compare);
+  return toRank(collection);
+
 }
 module.exports = rank_by_two_large_one_small;
